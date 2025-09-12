d5064d121c726af813a034083c0ab374bb95855b
predu <3pedrolucca@gmail.com>
2025-09-12 17:45:17 -0300

    feat: add button-navbar-responsive and fixing bug visual home page

--- src/Components/layouts/Navbar.tsx
+++ src/Components/layouts/Navbar.tsx
@@ -1,53 +1,138 @@
-import { useState } from "react"
+import { useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
+import { motion, AnimatePresence, type Variants } from "framer-motion";
+import { exit } from "process";
 
 const NavLinks = [
-    { href: '#home', label: 'Home' },
-    { href: '#about', label: 'About Me' },
-    { href: '#contact', label: 'Contact' },
-]
+    { href: "#home", label: "Home" },
+    { href: "#about", label: "About Me" },
+    { href: "#stacks", label: "Stacks" },
+    { href: "#contact", label: "Contact" },
+];
 
 export default function Navbar() {
-    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
-    }
+    };
+
+    // 2. Adicionar o tipo 'Variants' nas constantes de animação
+    const menuVariants: Variants = {
+        hidden: { opacity: 0, y: -20 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                staggerChildren: 0.1,
+                ease: "easeInOut",
+            },
+        },
+        exit: { opacity: 0, y: -20, transition: { ease: "easeInOut" } },
+    };
+
+    const linkVariants: Variants = {
+        hidden: { opacity: 0, y: -10 },
+        visible: { opacity: 1, y: 0 },
+        exit: { opacity: 0, y: -10 },
+    };
+    {/*Definindo a animação botão navbar*/ }
+    const iconVariants: Variants = {
+        hidden: { opacity: 0, rotate: -90, scale: 0.8 }, // estado inicial
+        visible: {
+            opacity: 1, rotate: 0, scale: 1,
+            transition: { duration: 0.2, ease: 'easeOut' }
+        }, // estado final
+        exit: {
+            opacity: 0, rotate: 90, scale: 0.8,
+            transition: { duration: 0.5, ease: 'easeOut' }
+        } // ao desaparecer
+    };
 
     return (
-        <header className="fixed top-0 left-0 right-0 z-50">
-            <nav className="
-                mt-6 mx-auto max-w-6xl px-6 py-3
-                bg-black/30 backdrop-blur-sm
-                rounded-3xl
-                flex items-center justify-between"
-
-            >
+        <header className="fixed top-0 left-0 right-0 z-50 px-4">
+            <nav className="relative mt-6 mx-auto max-w-6xl px-6 py-3 bg-black/30 backdrop-blur-lg rounded-3xl flex items-center justify-between">
                 {/* Logo */}
-                <a href="#home"
-                    className="flex-1 md:flex-none md:text-left">
-                    <span className="
-                    font-mono font-bold text-lg sm:text-xl
-                    text-transparent bg-clip-text
-                    bg-gradient-to-r from-cyan-400 via-pink-400 to-purple-500"
-                    >
+                <a href="#home" className="flex-1 md:flex-none md:text-left">
+                    <span className="font-mono font-bold text-lg sm:text-xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 via-pink-400 to-purple-500">
                         pedroLucca.dev();
                     </span>
                 </a>
-                {/* Links de Navegação Desktop */}
-                <div className="hidden xl:flex md:flex items-center gap-8">
+
+                {/* Links Desktop */}
+                <div className="hidden md:flex items-center gap-8">
                     {NavLinks.map((link) => (
                         <a
                             key={link.href}
                             href={link.href}
-                            onClick={toggleMenu} // Fecha o menu ao clicar em um link
-                            className=" text-md text-slate-200 hover:text-cyan-400 transition-colors font-custom"
+                            className="text-md text-slate-200 hover:text-cyan-400 transition-colors font-custom"
                         >
                             {link.label}
                         </a>
                     ))}
                 </div>
-                {/* Hamburguer Nav */}
+
+                {/* Botão Hambúrguer */}
+                <button
+                    onClick={toggleMenu}
+                    className="
+                        relative flex items-center justify-center md:hidden
+                        text-white z-50 h-7 w-7
+
+                    "
+                >
+                    {/* Gerenciador de Animações*/}
+                    <AnimatePresence mode="wait">
+                        {isMenuOpen ? (
+                            <motion.div
+                                key="close"
+                                variants={iconVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="exit"
+                                className="absolute"
+                            >
+                                <HiX size={28} />
+                            </motion.div>
+                        ) : (
+                            <motion.div
+                                key="menu"
+                                variants={iconVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="exit"
+                                className="absolute"
+                            >
+                                <HiMenu size={28} />
+                            </motion.div>
+                        )}
+                    </AnimatePresence>
+                </button>
+                {/* Quando o menu vai ser aberto */}
+                <AnimatePresence>
+                    {isMenuOpen && (
+                        <motion.div
+                            variants={menuVariants}
+                            initial="hidden"
+                            animate="visible"
+                            exit="exit"
+                            className="absolute top-14 left-6 right-6 bg-black/60 backdrop-blur-xl rounded-2xl p-5 flex flex-col items-start gap-6 md:hidden"
+                        >
+                            {NavLinks.map((link) => (
+                                <motion.a
+                                    key={link.href}
+                                    variants={linkVariants}
+                                    href={link.href}
+                                    onClick={toggleMenu}
+                                    className="text-lg text-slate-200 hover:text-cyan-400"
+                                >
+                                    {link.label}
+                                </motion.a>
+                            ))}
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </nav>
-        </header >
-    )
+        </header>
+    );
 }
--- src/Components/sections/HomePage.tsx
+++ src/Components/sections/HomePage.tsx
@@ -26,7 +26,7 @@ export function HomePage() {
                     text={[
                         "Computer Science Student",
                         "UI | UX Designer",
-                        "Full-Sack Developer",
+                        "Aspiring Full-Stack Developer",
                         "React Developer",
                     ]}
                     typingSpeed={75}
@@ -35,8 +35,8 @@ export function HomePage() {
                 />
 
                 {/* 2. Tipografia e espaçamento responsivo no Parágrafo */}
-                <p className="text-sm sm:text-base md:text-lg text-center font-custom text-slate-400 mt-5 px-2 sm:px-6">
-                    Full-Stack Developer and UI/UX Designer. With experience in projects for the universities <a href="https://sti.ufrn.br/" className="underline hover:text-cyan-500"> STI | UFRN </a> and <a href="https://ufg.br/" className="underline hover:text-cyan-500">UFG</a>, I specialize in transforming ideas into digital reality, combining clean code and intuitive interfaces.
+                <p className="text-sm sm:text-base md:text-lg text-center font-custom text-slate-300 mt-5 px-2 sm:px-6">
+                    Web Developer and UI/UX Designer. With experience in projects for the universities <a href="https://sti.ufrn.br/" className="underline hover:text-cyan-500"> STI | UFRN </a> and <a href="https://ufg.br/" className="underline hover:text-cyan-500">UFG</a>, I specialize in transforming ideas into digital reality, combining clean code and intuitive interfaces.
                 </p>
 
                 {/* 3. Espaçamento responsivo e botão adaptável */}
