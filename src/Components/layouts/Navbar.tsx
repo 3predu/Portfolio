a22a554dfc447964d90eade4dc5585d6b970e111
predu <3pedrolucca@gmail.com>
2025-09-17 20:34:58 -0300

    feat: adjustment-NavBar.tsx-color-and-spacing

--- src/Components/layouts/Navbar.tsx
+++ src/Components/layouts/Navbar.tsx
@@ -58,8 +58,8 @@ export default function Navbar() {
       <nav
         className="
                     relative mt-6 mx-auto max-w-6xl px-6 py-3 flex items-center justify-between
-                    backdrop-blur-lg bg-gray-950/50 rounded-full 
-                    shadow-[inset_0_1px_0_0_rgba(255,255,255,0.2),0_2px_8px_0_rgba(0,0,0,0.16)] border-white/20 border-[0.5px]
+                    backdrop-blur-lg bg-black/50 rounded-full 
+                    shadow-[inset_0_1px_0_0_rgba(255,255,255,0.2),0_2px_8px_0_rgba(0,0,0,0.16)] border-white/10 border-[0.5px]
                 "
       >
         {/* Logo */}
@@ -117,34 +117,34 @@ export default function Navbar() {
             )}
           </AnimatePresence>
         </button>
-        {/* Quando o menu vai ser aberto */}
-        <AnimatePresence>
-          {isMenuOpen && (
-            <motion.div
-              variants={menuVariants}
-              initial="hidden"
-              animate="visible"
-              exit="exit"
-              className="absolute top-14 left-6 right-6 backdrop-blur-xl rounded-2xl p-5 flex flex-col items-start gap-6 md:hidden
-                                mt-6 mx-auto max-w-6xl px-6 py-3 justify-between bg-gray-950/50
-                                shadow-[inset_0_1px_0_0_rgba(255,255,255,0.2),0_2px_8px_0_rgba(0,0,0,0.16)] border-white/20 border-[0.5px]
-                            "
-            >
-              {NavLinks.map((link) => (
-                <motion.a
-                  key={link.href}
-                  variants={linkVariants}
-                  href={link.href}
-                  onClick={toggleMenu}
-                  className="text-lg text-slate-200 hover:text-cyan-400"
-                >
-                  {link.label}
-                </motion.a>
-              ))}
-            </motion.div>
-          )}
-        </AnimatePresence>
       </nav>
+      {/* Quando o menu vai ser aberto */}
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.div
+            variants={menuVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            className="
+                      absolute top-14 left-6 right-6 backdrop-blur-xl rounded-2xl p- flex flex-col items-start gap-6 md:hidden
+                      mt-6 mx-auto max-w-6xl px-6 py-3 justify-between bg-black/80 border-white/10 border-[0.5px]
+            "
+          >
+            {NavLinks.map((link) => (
+              <motion.a
+                key={link.href}
+                variants={linkVariants}
+                href={link.href}
+                onClick={toggleMenu}
+                className="text-lg text-slate-200 hover:text-cyan-400"
+              >
+                {link.label}
+              </motion.a>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </header>
   );
 }
